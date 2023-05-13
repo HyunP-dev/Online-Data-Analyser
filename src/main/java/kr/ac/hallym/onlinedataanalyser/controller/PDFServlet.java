b5e405a240870825c8200626fab0a80fd66d3f96
Park Hyun <hyunp.dev@gmail.com>
2023-05-13 21:57:29 +0900

    upload

--- src/main/java/kr/ac/hallym/onlinedataanalyser/controller/PDFServlet.java
+++ src/main/java/kr/ac/hallym/onlinedataanalyser/controller/PDFServlet.java
@@ -4,9 +4,9 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.SneakyThrows;
 
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
@@ -15,12 +15,13 @@ public class PDFServlet extends HttpServlet {
     public void init() {
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    @SneakyThrows
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
         response.setContentType("application/pdf");
-        request.getParameter("");
-        InputStream in = new FileInputStream("/mnt/d_drive/University_Homeworks/2021 여름방학 프로젝트/3축가속도센서를이용한실시간걸음수검출알고리즘.pdf");
+        String filename = request.getParameter("filename");
+        if (filename.contains("..")) throw new Exception("공격을 감지하였습니다.");
+        InputStream in = new FileInputStream(System.getProperty("user.home") + "/Online-Data-Analyser-Data/" + filename);
         OutputStream out = response.getOutputStream();
-//        OutputStreamWriter writer = new OutputStreamWriter(out);
         byte[] buffer = new byte[1024];
         int numBytesRead;
         while ((numBytesRead = in.read(buffer)) > 0) {
---
+++ src/main/java/kr/ac/hallym/onlinedataanalyser/statistics/Correlation.java
@@ -0,0 +1,30 @@
+package kr.ac.hallym.onlinedataanalyser.statistics;
+
+import lombok.Getter;
+import org.rosuda.REngine.REXPMismatchException;
+import org.rosuda.REngine.REngine;
+import org.rosuda.REngine.REngineException;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class Correlation {
+    @Getter
+    private final double estimate;
+
+    @Getter
+    private final double statistic;
+
+    @Getter
+    private final double pValue;
+
+    public Correlation(double[] v1, double[] v2) throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, IllegalAccessException, REngineException, REXPMismatchException {
+        REngine engine = REngine.engineForClass("org.rosuda.REngine.JRI.JRIEngine");
+        engine.assign("v1", v1);
+        engine.assign("v2", v2);
+        engine.assign("t", engine.parseAndEval("cor.test(v1, v2)"));
+        estimate = engine.parseAndEval("t$estimate").asDouble();
+        statistic = engine.parseAndEval("t$statistic").asDouble();
+        pValue = engine.parseAndEval("t$p.value").asDouble();
+        engine.close();
+    }
+}
