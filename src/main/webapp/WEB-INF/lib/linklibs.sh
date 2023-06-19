cd0ae98e0f9bfb1531bf019720c58fa10dfd0e45
Park Hyun <hyunp.dev@gmail.com>
2023-06-19 16:18:30 +0900

    upload

---
+++ src/main/webapp/WEB-INF/lib/linklibs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ln -s /usr/local/lib/R/library/rJava/jri/REngine.jar REngine.jar
+ln -s /usr/local/lib/R/library/rJava/jri/JRI.jar JRI.jar
+ln -s /usr/local/lib/R/library/rJava/jri/JRIEngine.jar JRIEngine.jar
